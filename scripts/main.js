b64f1380c26f7f57e19c467494e83ad9ac03ac6d
chuckrak <chuckrak1@gmail.com>
2021-04-23 12:01:16 -0700

    Website Changes

--- scripts/main.js
+++ scripts/main.js
@@ -2,28 +2,28 @@ let myImage = document.querySelector('img');
 
 myImage.onclick = function() {
     let mySrc = myImage.getAttribute('src');
-    if (mySrc === 'images/cecil.jpeg') {
-        myImage.setAttribute('src', 'images/pomonalogo.png');
-    } else{
-        myImage.setAttribute('src', 'images/cecil.jpeg');
+    if (mySrc === 'images/ProfilePhotoSmall.png') {
+        myImage.setAttribute('src', 'images/IMG_1287.JPG');
+    } else {
+        myImage.setAttribute('src', 'images/ProfilePhotoSmall.png');
     }
 }
 
-let myButton = document.querySelector('button');
-let myHeading = document.querySelector('h1');
-function setUserName() {
-    let myName = prompt('Please enter your name:');
-    localStorage.setItem('name', myName);
-    myHeading.textContent = 'Welcome to Interact, ' + myName;
-}
+// let myButton = document.querySelector('button');
+// let myHeading = document.querySelector('h1');
+// function setUserName() {
+//     let myName = prompt('Please enter your name:');
+//     localStorage.setItem('name', myName);
+//     myHeading.textContent = 'Welcome to Interact, ' + myName;
+// }
 
-if(!localStorage.getItem('name')){
-    setUserName();
-} else{
-    let storedName = localStorage.getItem('name');
-    myHeading.textContent = 'Welcome to Interact, ' + storedName;
-}
+// if(!localStorage.getItem('name')){
+//     setUserName();
+// } else{
+//     let storedName = localStorage.getItem('name');
+//     myHeading.textContent = 'Welcome to Interact, ' + storedName;
+// }
 
-myButton.onclick = function() {
-    setUserName();
-}
+// myButton.onclick = function() {
+//     setUserName();
+// }
